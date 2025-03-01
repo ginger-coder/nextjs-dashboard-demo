1b41982667dec92e708b8ef97a185c058983bb8a
Bianxk <bxk0129@163.com>
2025-03-01 18:35:49 +0800

    feat: update dashboard

--- app/query/route.ts
+++ app/query/route.ts
@@ -1,26 +1,28 @@
-// import postgres from 'postgres';
+import postgres from "postgres";
 
-// const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
+const sql = postgres(process.env.POSTGRES_URL!, {
+    max: 20, // 最大连接数
+    idle_timeout: 30, // 空闲连接超时(秒)
+    connect_timeout: 10, // 连接超时(秒)
+    onnotice: (msg) => console.log(msg), // 处理通知
+    ssl: false, // SSL配置
+});
 
-// async function listInvoices() {
-// 	const data = await sql`
-//     SELECT invoices.amount, customers.name
-//     FROM invoices
-//     JOIN customers ON invoices.customer_id = customers.id
-//     WHERE invoices.amount = 666;
-//   `;
+async function listInvoices() {
+    const data = await sql`
+    SELECT invoices.amount, customers.name
+    FROM invoices
+    JOIN customers ON invoices.customer_id = customers.id
+    WHERE invoices.amount = 666;
+  `;
 
-// 	return data;
-// }
+    return data;
+}
 
 export async function GET() {
-  return Response.json({
-    message:
-      'Uncomment this file and remove this line. You can delete this file when you are finished.',
-  });
-  // try {
-  // 	return Response.json(await listInvoices());
-  // } catch (error) {
-  // 	return Response.json({ error }, { status: 500 });
-  // }
+    try {
+        return Response.json(await listInvoices());
+    } catch (error) {
+        return Response.json({ error }, { status: 500 });
+    }
 }
